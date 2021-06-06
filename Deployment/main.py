0b92afec2c8a4b1ec00bf0c89f32afc608ccee17
satyawiz <satyawiz@gmail.com>
2021-06-06 09:43:34 +0530

    'deployment'

---
+++ Deployment/main.py
@@ -0,0 +1,24 @@
+from logging import debug
+from flask import Flask , render_template,request
+import joblib
+app = Flask(__name__)
+
+model=joblib.load('hiring_model.pkl')
+@app.route('/')
+def welcome():
+    return render_template('base.html')
+
+@app.route('/predict' , methods = ['POST'])
+def predict():
+
+    exp = request.form.get('experience')
+    score = request.form.get('test_score')
+    interview_score = request.form.get('Interview_score')
+
+    prediction = model.predict([[int(exp) , int(score) , int(interview_score)]])
+
+    output = round(prediction[0] , 2)
+
+    return render_template('base.html' , prediction_text = f"Employee Salary will be $ {output}")
+
+app.run(debug=True)
